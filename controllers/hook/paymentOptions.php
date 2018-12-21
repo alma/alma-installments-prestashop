8b83fc8cada7751a038193e8e73fcaf40fdc290b
Olivier Lance <olivier.lance@getalma.eu>
2018-12-21 17:18:21 +0100

    Use tiny logo for payment method display

--- controllers/hook/paymentOptions.php
+++ controllers/hook/paymentOptions.php
@@ -76,7 +76,7 @@ class AlmaPaymentOptionsController extends AlmaProtectedHookController
             ->setCallToActionText(AlmaSettings::getPaymentButtonTitle())
             ->setAdditionalInformation($template)
             ->setAction($this->context->link->getModuleLink($this->module->name, 'payment', array(), true))
-            ->setLogo(Media::getMediaPath(_PS_MODULE_DIR_.$this->module->name.'/views/img/logo_full.png'));
+            ->setLogo(Media::getMediaPath(_PS_MODULE_DIR_.$this->module->name.'/views/img/tiny_logo.png'));
 
         return array($paymentOption);
     }
