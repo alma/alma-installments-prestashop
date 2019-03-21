3c11fabbe707b1ec671f3abd28e471990d5ab70b
Olivier Lance <olivier.lance@getalma.eu>
2019-03-21 16:57:48 +0100

    Script to zip the module properly

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DIR=`pwd`
+
+rm -rf ./dist/
+rm -rf /tmp/alma-build/alma
+mkdir -p /tmp/alma-build/alma
+
+cp -r ./* /tmp/alma-build/alma/
+
+mkdir ./dist
+
+cd /tmp/alma-build/
+zip -9 -r "$DIR/dist/alma.zip" alma --exclude \*dist\* \*.git\* \*.idea\* \*.DS_Store
+
+rm -rf /tmp/alma-build
