09a7600112d0544c21aa1c9ff320908a2c5e4d78
Olivier Lance <olivier.lance@getalma.eu>
2021-01-21 17:12:58 +0100

    fix: change UNPKG urls to .com instead of .io (apparently a test domain...)

--- alma/controllers/hook/FrontHeaderHookController.php
+++ alma/controllers/hook/FrontHeaderHookController.php
@@ -76,8 +76,8 @@ final class FrontHeaderHookController extends FrontendHookController
             return null;
         }
 
-        $widgetsCssUrl = 'https://unpkg.io/@alma/widgets@1.x.x/dist/alma-widgets.css';
-        $widgetsJsUrl = 'https://unpkg.io/@alma/widgets@1.x.x/dist/alma-widgets.umd.js';
+        $widgetsCssUrl = 'https://unpkg.com/@alma/widgets@1.x.x/dist/alma-widgets.css';
+        $widgetsJsUrl = 'https://unpkg.com/@alma/widgets@1.x.x/dist/alma-widgets.umd.js';
         $productScriptPath = 'views/js/alma-product.js';
         $productCssPath = 'views/css/alma-product.css';
 
