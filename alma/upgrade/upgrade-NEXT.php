d31a5ac8285159de9d6a7c328625852b13e7b09e
Arnaud Rolland <arnaud.rolland@getalma.eu>
2021-02-18 15:20:51 +0100

    prepare upgrade file

--- alma/upgrade/upgrade-NEXT.php
+++ alma/upgrade/upgrade-NEXT.php
@@ -29,7 +29,7 @@ if (!defined('_PS_VERSION_')) {
 
 function upgrade_module_NEXT($module)
 {
-    Configuration::deleteByName('ALMA_IS_ELIGIBLE_MESSAGE');
+    ConfigurationCore::deleteByName('ALMA_IS_ELIGIBLE_MESSAGE');
 
     return true;
 }
