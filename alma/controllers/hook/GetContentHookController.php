ca358bb121c4bde9d101ccb16e518fedecf68935
Benjamin Freoua <89775252+Benjamin-Freoua-Alma@users.noreply.github.com>
2021-10-04 16:48:03 +0200

    add conditions for deferred_days and deferred_months

--- alma/controllers/hook/GetContentHookController.php
+++ alma/controllers/hook/GetContentHookController.php
@@ -180,6 +180,8 @@ final class GetContentHookController extends AdminHookController
                 $feePlans = $this->getFeePlans();
                 foreach ($feePlans as $feePlan) {
                     $n = $feePlan->installments_count;
+                    $deferred_days = $feePlan->deferred_days;
+                    $deferred_months = $feePlan->deferred_months;
                     $key = Settings::keyForFeePlan($feePlan);
                     if (1 == $n && !Settings::isDeferred($feePlan)) {
                         continue;
@@ -196,6 +198,8 @@ final class GetContentHookController extends AdminHookController
                         $this->context->smarty->assign([
                             'validation_error' => 'pnx_min_amount',
                             'n' => $n,
+                            'deferred_days' => $deferred_days,
+                            'deferred_months' => $deferred_months,
                             'min' => almaPriceFromCents($feePlan->min_purchase_amount),
                             'max' => almaPriceFromCents(min($max, $feePlan->max_purchase_amount)),
                         ]);
@@ -207,6 +211,8 @@ final class GetContentHookController extends AdminHookController
                         $this->context->smarty->assign([
                             'validation_error' => 'pnx_max_amount',
                             'n' => $n,
+                            'deferred_days' => $deferred_days,
+                            'deferred_months' => $deferred_months,
                             'min' => almaPriceFromCents($min),
                             'max' => almaPriceFromCents($feePlan->max_purchase_amount),
                         ]);
