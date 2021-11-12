73d1b8e15fb2cc699a1815eb1c659225e5f471bf
Benjamin Freoua <benjamin.freoua@getalma.eu>
2021-11-12 14:59:35 +0100

    add upgrade file for fix for all merchant

---
+++ alma/upgrade/upgrade-NEXT.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * 2018-2021 Alma SAS
+ *
+ * THE MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @author    Alma SAS <contact@getalma.eu>
+ * @copyright 2018-2021 Alma SAS
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+include_once _PS_MODULE_DIR_ . 'alma/vendor/autoload.php';
+
+use Alma\API\RequestError;
+use Alma\PrestaShop\API\ClientHelper;
+use Alma\PrestaShop\Utils\Settings;
+
+function upgrade_module_2_3_0($module)
+{
+    if (Settings::isFullyConfigured()) {
+        $alma = ClientHelper::defaultInstance();
+
+        if (!$alma) {
+            return true;
+        }
+
+        $configKeys = [
+            'ALMA_PAYMENT_BUTTON_TITLE',
+            'ALMA_PAYMENT_BUTTON_DESC',
+            'ALMA_DEFERRED_BUTTON_TITLE',
+            'ALMA_DEFERRED_BUTTON_DESC',
+            'ALMA_NOT_ELIGIBLE_CATEGORIES',
+        ];
+
+        try {
+            foreach ($configKeys as $configKey) {
+                Configuration::deleteByName($configKey);
+            }
+
+            Settings::initCustomFields();
+        } catch (RequestError $e) {
+            return true;
+        }
+    }
+
+    return true;
+}
