0807b57cba057d9e4d3e4dc0b4a2e17573ff0585
Benjamin Freoua <benjamin.freoua@getalma.eu>
2021-11-23 18:20:00 +0100

    less p10x in fragment + opti js files fragment

--- alma/controllers/front/payment.php
+++ alma/controllers/front/payment.php
@@ -25,6 +25,7 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
+use Alma\API\RequestError;
 use Alma\PrestaShop\API\ClientHelper;
 use Alma\PrestaShop\Model\PaymentData;
 use Alma\PrestaShop\Utils\Logger;
