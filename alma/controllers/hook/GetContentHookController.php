0704bddff791f45ff97da3007ac47d5b830f09bd
Benjamin Freoua <benjamin.freoua@getalma.eu>
2021-11-25 16:40:51 +0100

    add option for activate frangment

--- alma/controllers/hook/GetContentHookController.php
+++ alma/controllers/hook/GetContentHookController.php
@@ -258,6 +258,9 @@ final class GetContentHookController extends AdminHookController
 
             $descriptionPaymentTrigger = Tools::getValue('ALMA_DESCRIPTION_TRIGGER');
             Settings::updateValue('ALMA_DESCRIPTION_TRIGGER', $descriptionPaymentTrigger);
+            
+            $activateFragment = (bool) Tools::getValue('ALMA_ACTIVATE_FRAGMENT_ON');
+            Settings::updateValue('ALMA_ACTIVATE_FRAGMENT', $activateFragment);
 
             $activateLogging = (bool) Tools::getValue('ALMA_ACTIVATE_LOGGING_ON');
             Settings::updateValue('ALMA_ACTIVATE_LOGGING', $activateLogging);
