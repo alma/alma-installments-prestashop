cc618130569c968adc8ba714c2551005e78c2081
Benjamin Freoua <benjamin.freoua@getalma.eu>
2021-12-15 15:46:06 +0100

    delete and report testUnit customfield on new branch

--- alma/tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('_PS_VERSION_', 'toto');
-
-require_once __DIR__ . '/../vendor/autoload.php';
--- scripts/unittest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO : Need optimization. Can be exec without bootstrap I think
-
-../alma/vendor/bin/pest --bootstrap tests/bootstrap.php tests/Unit/lib/Utils/SettingsCustomFieldsTest.php
