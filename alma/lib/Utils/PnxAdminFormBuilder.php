e1fe0d7357fcd67d4eb846a59e68ca1de163a53e
Sylvain Just <sy.just@i-sloth.com>
2022-01-14 15:54:33 +0100

    refact: reduce PnxAdminForm complexity

--- alma/lib/Utils/PnxAdminFormBuilder.php
+++ alma/lib/Utils/PnxAdminFormBuilder.php
@@ -24,6 +24,8 @@
 
 namespace Alma\PrestaShop\Utils;
 
+use Alma\API\Entities\FeePlan;
+
 if (!defined('_PS_VERSION_')) {
     exit;
 }
@@ -35,117 +37,99 @@ if (!defined('_PS_VERSION_')) {
  */
 class PnxAdminFormBuilder extends AbstractAlmaAdminFormBuilder
 {
-    protected function configForm() {
-        $return = [];
-        $pnxTabs = [];
-        $activeTab = null;
-        $installmentsPlans = $this->config['installmentsPlans'];
+    /**
+     * @param FeePlan $feePlan
+     * @param int     $duration
+     *
+     * @return array
+     */
+    protected function buildPnxForm(FeePlan $feePlan, $duration)
+    {
+        $tabId = $key = $feePlan->getPlanKey();
 
-        foreach ($this->config['feePlans'] as $feePlan) {
-            $key = Settings::keyForFeePlan($feePlan);
-            if (1 == $feePlan->installments_count && !Settings::isDeferred($feePlan)) {
-                continue;
-            }
-
-            // Disable and hide disallowed fee plans
-            if (!$feePlan->allowed) {
-                unset($installmentsPlans->$key);
-                Settings::updateValue('ALMA_FEE_PLANS', json_encode($installmentsPlans));
-
-                continue;
-            }
-
-            $tabId = $key;
-
-            // PrestaShop won't detect the string if the call to `l` is multiline
-            // phpcs:ignore
-            $tabTitle = sprintf($this->module->l('%d-installment payments', 'GetContentHookController'), $feePlan->installments_count);
-            $duration = Settings::getDuration($feePlan);
-            $label = sprintf(
-                $this->module->l('Enable %d-installment payments', 'GetContentHookController'),
-                $feePlan->installments_count
-            );
-            if (Settings::isDeferred($feePlan)) {
-                if ($feePlan->installments_count == 1) {
-                    // PrestaShop won't detect the string if the call to `l` is multiline
-                    // phpcs:ignore
-                    $tabTitle = sprintf($this->module->l('Deferred payments + %d days', 'GetContentHookController'), $duration);
-                    $label = sprintf(
-                        $this->module->l('Enable deferred payments +%d days', 'GetContentHookController'),
-                        $duration
-                    );
-                } else {
-                    // PrestaShop won't detect the string if the call to `l` is multiline
-                    // phpcs:ignore
-                    $tabTitle = sprintf($this->module->l('%d-installment payments + %d-deferred days', 'GetContentHookController'), $feePlan->installments_count, $duration);
-                    $label = sprintf(
-                        // PrestaShop won't detect the string if the call to `l` is multiline
-                        // phpcs:ignore
-                        $this->module->l('Enable %d-installment payments +%d-deferred days', 'GetContentHookController'),
-                        $feePlan->installments_count,
-                        $duration
-                    );
-                }
-            }
-
-            $enable = isset($installmentsPlans->$key->enabled) ? $installmentsPlans->$key->enabled : 0;
-            if (1 == $enable) {
-                $pnxTabs[$tabId] = '✅ ' . $tabTitle;
-                $activeTab = $activeTab ?: $tabId;
-            } else {
-                $pnxTabs[$tabId] = '❌ ' . $tabTitle;
-            }
-
-            $minAmount = (int) almaPriceFromCents($feePlan->min_purchase_amount);
-            $maxAmount = (int) almaPriceFromCents($feePlan->max_purchase_amount);
-
-            $tpl = $this->context->smarty->createTemplate(
-                "{$this->module->local_path}views/templates/hook/pnx_fees.tpl"
-            );
-            $tpl->assign(
-                ['fee_plan' => (array) $feePlan,
-                'min_amount' => $minAmount,
-                'max_amount' => $maxAmount,
-                'deferred' => $duration, ]
-            );
-
-            array_push($return,
-                $this->inputHtml($tpl, null, "$tabId-content"),
-                $this->inputAlmaSwitchForm(
-                    "ALMA_${key}_ENABLED",
-                    $label,
-                    null,
-                    null,
-                    "$tabId-content"
-                ),
-                $this->inputNumberForm(
-                    "ALMA_${key}_MIN_AMOUNT",
-                    $this->module->l('Minimum amount (€)', 'GetContentHookController'),
-                    $this->module->l('Minimum purchase amount to activate this plan', 'GetContentHookController'),
-                    $minAmount,
-                    $maxAmount,
-                    "$tabId-content"
-                ),
-                $this->inputNumberForm(
-                    "ALMA_${key}_MAX_AMOUNT",
-                    $this->module->l('Maximum amount (€)', 'GetContentHookController'),
-                    $this->module->l('Maximum purchase amount to activate this plan', 'GetContentHookController'),
-                    $minAmount,
-                    $maxAmount,
-                    "$tabId-content"
-                ),
-                $this->inputNumberForm(
-                    "ALMA_${key}_SORT_ORDER",
-                    $this->module->l('Position', 'GetContentHookController'),
-                    $this->module->l('Use relative values to set the order on the checkout page', 'GetContentHookController'),
-                    null,
-                    null,
-                    "$tabId-content"
-                )
-            );
-        }
+        $minAmount = (int) almaPriceFromCents($feePlan->min_purchase_amount);
+        $maxAmount = (int) almaPriceFromCents($feePlan->max_purchase_amount);
 
         $tpl = $this->context->smarty->createTemplate(
+            "{$this->module->local_path}views/templates/hook/pnx_fees.tpl"
+        );
+        $tpl->assign(
+            [
+                'fee_plan'   => (array) $feePlan,
+                'min_amount' => $minAmount,
+                'max_amount' => $maxAmount,
+                'deferred'   => $duration,
+            ]
+        );
+
+        return [
+            $this->inputHtml($tpl, null, "$tabId-content"),
+            $this->inputAlmaSwitchForm(
+                "ALMA_${key}_ENABLED",
+                $this->getLabel($feePlan, $duration),
+                null,
+                null,
+                "$tabId-content"
+            ),
+            $this->inputNumberForm(
+                "ALMA_${key}_MIN_AMOUNT",
+                $this->module->l('Minimum amount (€)', 'GetContentHookController'),
+                $this->module->l('Minimum purchase amount to activate this plan', 'GetContentHookController'),
+                $minAmount,
+                $maxAmount,
+                "$tabId-content"
+            ),
+            $this->inputNumberForm(
+                "ALMA_${key}_MAX_AMOUNT",
+                $this->module->l('Maximum amount (€)', 'GetContentHookController'),
+                $this->module->l('Maximum purchase amount to activate this plan', 'GetContentHookController'),
+                $minAmount,
+                $maxAmount,
+                "$tabId-content"
+            ),
+            $this->inputNumberForm(
+                "ALMA_${key}_SORT_ORDER",
+                $this->module->l('Position', 'GetContentHookController'),
+                $this->module->l(
+                    'Use relative values to set the order on the checkout page',
+                    'GetContentHookController'
+                ),
+                null,
+                null,
+                "$tabId-content"
+            ),
+        ];
+    }
+
+    protected function configForm()
+    {
+        $return            = [];
+        $pnxTabs           = [];
+        $activeTab         = null;
+        $installmentsPlans = $this->config['installmentsPlans'];
+
+        /** @var FeePlan $feePlan */
+        foreach ($this->config['feePlans'] as $feePlan) {
+            $tabId = $key = $feePlan->getPlanKey();
+            if (!$feePlan->isPayLaterOnly() && !$feePlan->isPnXOnly()) {
+                continue;
+            }
+
+            if (!$feePlan->allowed) {
+                $this->disableFeePlan($key, $installmentsPlans);
+                continue;
+            }
+            $duration = Settings::getDuration($feePlan);
+            array_merge($return, $this->buildPnxForm($feePlan, $duration));
+            $pnxTabs[$tabId] = '❌ ';
+            if ($this->isEnabled($key, $installmentsPlans)) {
+                $pnxTabs[$tabId] = '✅ ';
+                $activeTab       = $activeTab ?: $tabId;
+            }
+            $pnxTabs[$tabId] .= $this->getTabTitle($feePlan, $duration);
+        }
+
+        $tpl       = $this->context->smarty->createTemplate(
             "{$this->module->local_path}views/templates/hook/pnx_tabs.tpl"
         );
         $forceTabs = false;
@@ -157,9 +141,9 @@ class PnxAdminFormBuilder extends AbstractAlmaAdminFormBuilder
         array_unshift(
             $return,
             [
-                'name' => null,
-                'label' => null,
-                'type' => 'html',
+                'name'         => null,
+                'label'        => null,
+                'type'         => 'html',
                 'html_content' => $tpl->fetch(),
             ]
         );
@@ -167,8 +151,98 @@ class PnxAdminFormBuilder extends AbstractAlmaAdminFormBuilder
         return $return;
     }
 
+    /**
+     * @param $key
+     * @param $installmentsPlans
+     *
+     * @return void
+     */
+    protected function disableFeePlan($key, $installmentsPlans)
+    {
+        unset($installmentsPlans->$key);
+        Settings::updateValue('ALMA_FEE_PLANS', json_encode($installmentsPlans));
+    }
+
+    /**
+     * @param FeePlan $feePlan
+     * @param int     $duration
+     *
+     * @return string
+     */
+    private function getLabel(FeePlan $feePlan, $duration)
+    {
+        if ($feePlan->isPnXOnly()) {
+            return sprintf(
+                $this->module->l('Enable %d-installment payments', 'GetContentHookController'),
+                $feePlan->installments_count
+            );
+        }
+        if ($feePlan->isPayLaterOnly()) {
+            // PrestaShop won't detect the string if the call to `l` is multiline
+            // phpcs:ignore
+            return sprintf(
+                $this->module->l('Enable deferred payments +%d days', 'GetContentHookController'),
+                $duration
+            );
+        }
+
+        return sprintf(
+        // PrestaShop won't detect the string if the call to `l` is multiline
+        // phpcs:ignore
+            $this->module->l('Enable %d-installment payments +%d-deferred days', 'GetContentHookController'),
+            $feePlan->installments_count,
+            $duration
+        );
+    }
+
+    /**
+     * @param FeePlan $feePlan
+     * @param int     $duration
+     *
+     * @return string
+     */
+    protected function getTabTitle(FeePlan $feePlan, $duration)
+    {
+        if ($feePlan->isPnXOnly()) {
+            return sprintf(
+                $this->module->l('%d-installment payments', 'GetContentHookController'),
+                $feePlan->installments_count
+            );
+        }
+
+        if ($feePlan->isPayLaterOnly()) {
+            // PrestaShop won't detect the string if the call to `l` is multiline
+            // phpcs:ignore
+            return sprintf(
+                $this->module->l('Deferred payments + %d days', 'GetContentHookController'),
+                $duration
+            );
+        }
+
+        // PrestaShop won't detect the string if the call to `l` is multiline
+        // phpcs:ignore
+        return sprintf(
+            $this->module->l('%d-installment payments + %d-deferred days', 'GetContentHookController'),
+            $feePlan->installments_count,
+            $duration
+        );
+    }
+
     protected function getTitle()
     {
         return $this->module->l('Installments plans', 'GetContentHookController');
+    }
+
+    /**
+     * @param $key
+     * @param $installmentsPlans
+     *
+     * @return bool
+     */
+    protected function isEnabled($key, $installmentsPlans)
+    {
+        $enable = isset($installmentsPlans->$key->enabled) ? $installmentsPlans->$key->enabled : 0;
+
+        return $enable == 1;
     }
 }
