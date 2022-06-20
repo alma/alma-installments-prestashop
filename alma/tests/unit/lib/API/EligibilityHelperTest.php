533173445a69bb7e90150182273cf214a2ee47a1
Benjamin Freoua <benjamin.freoua@getalma.eu>
2022-06-20 19:03:23 +0200

    base files unit test

---
+++ alma/tests/unit/lib/API/EligibilityHelperTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Alma\PrestaShop\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class EligibilityHelperTest extends TestCase
+{
+    public function testGetEligibleFeePlans()
+    {
+        
+    }
+}
