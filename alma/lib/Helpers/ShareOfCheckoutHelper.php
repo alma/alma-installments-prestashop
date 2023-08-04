6814c2a783fef565a38b5822385ccdd037cfd5c4
Benjamin Freoua <benjamin.freoua@getalma.eu>
2023-08-04 14:59:01 +0200

    sonar: Move this open curly brace to the beginning of the next line.

--- alma/lib/Helpers/ShareOfCheckoutHelper.php
+++ alma/lib/Helpers/ShareOfCheckoutHelper.php
@@ -52,7 +52,8 @@ class ShareOfCheckoutHelper
      */
     protected $context;
 
-    public function __construct($orderHelper) {
+    public function __construct($orderHelper)
+    {
         $this->orderHelper = $orderHelper;
         $this->context = \Context::getContext();
     }
