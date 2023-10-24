2b7df3819831798effef2e68727b3f25f0d889d3
Rémi Zuffinetti <remi.zuffinetti@getalma.eu>
2023-10-24 17:21:24 +0200

    Build using Docker

---
+++ scripts/build/build-dist-docker.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Build alma.zip from the module's source directory
+# Should be run from the project's source
+
+SRC_DIR=$(pwd)
+BUILD_DIR="/tmp/build/alma"
+
+mkdir -p $BUILD_DIR
+cp CHANGELOG.md $SRC_DIR/alma/
+cp -r $SRC_DIR/alma/* $BUILD_DIR
+
+if [ ! -d "./dist" ]; then
+  mkdir ./dist
+fi
+
+cd $BUILD_DIR
+
+rm -f composer.lock config.xml config_*.xml
+rm -rf tests vendor 
+
+php /usr/bin/composer install --no-dev --optimize-autoloader
+php /usr/bin/composer dump-autoload --optimize
+php vendor/bin/autoindex prestashop:add:index
+
+cd ..
+
+zip -9 -r "$SRC_DIR/dist/alma.zip" alma --exclude  "*/.*" "*/build.sh" "*/dist" "*/docker*"
+
+rm -rf $BUILD_DIR
