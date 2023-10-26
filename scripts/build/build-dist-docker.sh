9a2201bedaf9785a6222534d1e18498758b3f65c
Benjamin Freoua <benjamin.freoua@getalma.eu>
2023-10-26 17:49:10 +0200

    update builder for validator

--- scripts/build/build-dist-docker.sh
+++ scripts/build/build-dist-docker.sh
@@ -8,6 +8,7 @@ BUILD_DIR="/tmp/build/alma"
 mkdir -p $BUILD_DIR
 cp CHANGELOG.md $SRC_DIR/alma/
 cp -r $SRC_DIR/alma/* $BUILD_DIR
+cp $SRC_DIR/alma/.htaccess $BUILD_DIR/
 
 if [ ! -d "./dist" ]; then
   mkdir ./dist
@@ -15,7 +16,7 @@ fi
 
 cd $BUILD_DIR
 
-rm -f composer.lock config.xml config_*.xml
+rm -f composer.lock config.xml config_*.xml phpunit.ci.xml phpunit.dist.xml
 rm -rf tests vendor 
 
 php /usr/bin/composer install --no-dev --optimize-autoloader
@@ -24,6 +25,7 @@ php vendor/bin/autoindex prestashop:add:index
 
 cd ..
 
-zip -9 -r "$SRC_DIR/dist/alma.zip" alma --exclude  "*/.*" "*/build.sh" "*/dist" "*/docker*"
+zip -9 -r "$SRC_DIR/dist/alma.zip" alma --exclude "*/*/.*" "*/build.sh" "*/dist" "*/docker*"
 
 rm -rf $BUILD_DIR
+rm -rf "$SRC_DIR/alma/CHANGELOG.md"
