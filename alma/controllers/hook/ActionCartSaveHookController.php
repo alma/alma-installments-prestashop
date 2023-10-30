f274624bff987f6d8a06d1a159ed3761287dabe4
ClaireAlma <claire.bonachera@getalma.eu>
2023-10-30 16:02:40 +0100

    suite

--- alma/controllers/hook/ActionCartSaveHookController.php
+++ alma/controllers/hook/ActionCartSaveHookController.php
@@ -29,6 +29,7 @@ if (!defined('_PS_VERSION_')) {
 }
 
 use Alma\PrestaShop\Exceptions\InsuranceNotFoundException;
+use Alma\PrestaShop\Helpers\InsuranceHelper;
 use Alma\PrestaShop\Hooks\FrontendHookController;
 use Alma\PrestaShop\Repositories\AlmaInsuranceProductRepository;
 use Alma\PrestaShop\Repositories\ProductRepository;
@@ -59,6 +60,11 @@ class ActionCartSaveHookController extends FrontendHookController
      */
     protected $almaInsuranceProductRepository;
 
+    /**
+     * @var InsuranceHelper
+     */
+    protected $insuranceHelper;
+
     public function __construct($module)
     {
         parent::__construct($module);
@@ -67,6 +73,7 @@ class ActionCartSaveHookController extends FrontendHookController
         $this->attributeGroupRepository = new AttributeGroupRepository();
         $this->attributeRepository = new AttributeRepository();
         $this->almaInsuranceProductRepository = new AlmaInsuranceProductRepository();
+        $this->insuranceHelper = new InsuranceHelper();
     }
 
     /**
@@ -82,106 +89,60 @@ class ActionCartSaveHookController extends FrontendHookController
             isset($_POST['alma_insurance_price'])
             && $_POST['alma_insurance_price'] != 'none'
         ) {
-            // @todo Check elibilibilty
-
-            $value = $_POST['alma_insurance_price'];
-            $values = explode('-', $value);
-            $insurancePrice = $values[0];
-            $insuranceName = $values[1];
-
-            $insuranceProductId = $this->productRepository->getProductIdByReference(
-                ConstantsHelper::ALMA_INSURANCE_PRODUCT_REFERENCE,
-                $this->context->language->id
-            );
-
-            if(!$insuranceProductId) {
-                // @todo la recréer ? envoyer un message
-                throw new InsuranceNotFoundException();
-            }
-
-            /**
-             * @var \ProductCore $defaultInsuranceProduct
-             */
-            $defaultInsuranceProduct = new \Product((int)$insuranceProductId);
-
-            $attributeGroupId = $this->attributeGroupRepository->getAttributeIdByName(
-                ConstantsHelper::ALMA_INSURANCE_ATTRIBUTE_NAME,
-                $this->context->language->id
-            );
-
-            if(!$attributeGroupId) {
-                // @todo la recréer ? envoyer un message
-                throw new InsuranceNotFoundException();
-            }
-
-            $insuranceAttributeId = $this->attributeRepository->getAttributeIdByNameAndGroup(
-                $insuranceName,
-                $attributeGroupId,
-                $this->context->language->id
-            );
-
-            if(!$insuranceAttributeId) {
-                /**
-                 * @var \AttributeCore $testNewAttribute
-                 */
-                $insuranceAttribute = new \AttributeCore();
-
-                $insuranceAttribute->name = \PrestaShop\PrestaShop\Adapter\Import\ImportDataFormatter::createMultiLangField($insuranceName);
-                $insuranceAttribute->id_attribute_group = $attributeGroupId;
-                $insuranceAttribute->add();
-
-                $insuranceAttributeId = $insuranceAttribute->id;
-            }
-
-            // Check if the combination already exists
-
-            /**
-             * @var \CombinationCore $combinaison
-             */
-            $idProductAttribute =   \CombinationCore::getIdByReference($insuranceProductId, $insuranceName );
-
-            if(! $idProductAttribute) {
-                $idProductAttribute = $defaultInsuranceProduct->addCombinationEntity(
-                    $insurancePrice,
-                    $insurancePrice,
-                    0,
-                    1,
-                    0,
-                    1,
-                    0,
-                    $insuranceName,
-                    0,
-                    '',
-                    0
-                );
-            }
-
-            $combinaison = new \CombinationCore((int)$idProductAttribute);
-            $combinaison->setAttributes(array($insuranceAttributeId));
-
-            \StockAvailable::setQuantity($defaultInsuranceProduct->id, $idProductAttribute, 1, $this->context->shop->id);
-
-            $_POST['alma_insurance_price']  = 'none';
-
-            $this->context->cart->updateQty(1, $defaultInsuranceProduct->id, $idProductAttribute);
-
-            $product = $this->context->cart->getLastProduct();
-
-            $this->almaInsuranceProductRepository->add(
-                $this->context->cart->id,
-                $product['id_product'],
-                $this->context->shop->id,
-                $product['id_product_attribute'],
-                $_POST['id_customization'],
-                $insuranceProductId,
-                $insuranceAttributeId,
-                $insurancePrice
-            );
+            $this->addInsuranceToCart($_POST['alma_insurance_price']);
         }
-
-        $_POST['alma_insurance_price']  = 'none';
 
         // @todo suppression
     }
 
+    protected function addInsuranceToCart($insuranceData)
+    {
+        // @todo Check elibilibilty
+
+        // This code is temporary until the product widget is done
+        $values = explode('-', $insuranceData);
+        $insurancePrice = $values[0];
+        $insuranceName = $values[1];
+        // !!
+
+        // Get the default product insurance
+        $defaultInsuranceProduct = $this->insuranceHelper->getDefaultInsuranceProduct($this->context->language->id);
+        // Get the default group attribute id
+        $attributeGroupId =  $this->insuranceHelper->getInsuranceAttributeGroupId($this->context->language->id);
+        // Get or create the attribute (type of insurance choose)
+        $insuranceAttributeId = $this->insuranceHelper->createOrGetInsuranceAttributeId(
+            $insuranceName,
+            $attributeGroupId,
+            $this->context->language->id
+        );
+
+        // Create the combination of product + attribute
+        $combination = $this->insuranceHelper->createOrGetInsuranceCombination(
+            $defaultInsuranceProduct,
+            $insuranceName,
+            $insurancePrice,
+            $insuranceAttributeId
+        );
+
+        \StockAvailableCore::setQuantity($defaultInsuranceProduct->id, $combination->id, 1, $this->context->shop->id);
+
+        // We reset the data
+        $_POST['alma_insurance_price']  = 'none';
+
+        // Add the insurance to the cart
+        $this->context->cart->updateQty(1, $defaultInsuranceProduct->id, $combination->id);
+
+        $product = $this->context->cart->getLastProduct();
+
+        $this->almaInsuranceProductRepository->add(
+            $this->context->cart->id,
+            $product['id_product'],
+            $this->context->shop->id,
+            $product['id_product_attribute'],
+            $_POST['id_customization'],
+            $defaultInsuranceProduct->id,
+            $insuranceAttributeId,
+            $insurancePrice
+        );
+    }
 }
--- alma/lib/Helpers/InsuranceHelper.php
+++ alma/lib/Helpers/InsuranceHelper.php
@@ -24,8 +24,36 @@
 
 namespace Alma\PrestaShop\Helpers;
 
+use Alma\PrestaShop\Exceptions\InsuranceNotFoundException;
+use Alma\PrestaShop\Repositories\AttributeGroupRepository;
+use Alma\PrestaShop\Repositories\AttributeRepository;
+use Alma\PrestaShop\Repositories\ProductRepository;
+
 class InsuranceHelper
 {
+    /**
+     * @var AttributeGroupRepository
+     */
+    protected $attributeGroupRepository;
+
+
+    /**
+     * @var ProductRepository
+     */
+    protected $productRepository;
+
+    /**
+     * @var AttributeRepository
+     */
+    protected $attributeRepository;
+
+    public function __construct()
+    {
+        $this->productRepository = new ProductRepository();
+        $this->attributeGroupRepository = new AttributeGroupRepository();
+        $this->attributeRepository = new AttributeRepository();
+    }
+
     /**
      * @return bool
      */
@@ -34,5 +62,116 @@ class InsuranceHelper
         return (bool) (int) SettingsHelper::get(ConstantsHelper::ALMA_SHOW_INSURANCE_WIDGET_PRODUCT, false)
             && (bool) (int) SettingsHelper::get(ConstantsHelper::ALMA_ALLOW_INSURANCE, false)
             && (bool) (int) SettingsHelper::get(ConstantsHelper::ALMA_ACTIVATE_INSURANCE, false);
+    }
+
+
+    /**
+     * @param int $languageId
+     * @return \ProductCore
+     * @throws InsuranceNotFoundException
+     */
+    public function getDefaultInsuranceProduct($languageId)
+    {
+        $insuranceProductId = $this->productRepository->getProductIdByReference(
+            ConstantsHelper::ALMA_INSURANCE_PRODUCT_REFERENCE,
+            $languageId
+        );
+
+        if(!$insuranceProductId) {
+            // @todo la recréer ? envoyer un message
+            throw new InsuranceNotFoundException();
+        }
+
+        /**
+         * @var \ProductCore $defaultInsuranceProduct
+         */
+        return new \ProductCore((int)$insuranceProductId);
+    }
+
+    /**
+     * @param int $languageId
+     * @return string
+     * @throws InsuranceNotFoundException
+     */
+    public function getInsuranceAttributeGroupId($languageId) {
+        $attributeGroupId = $this->attributeGroupRepository->getAttributeIdByName(
+            ConstantsHelper::ALMA_INSURANCE_ATTRIBUTE_NAME,
+            $languageId
+        );
+
+        if(!$attributeGroupId) {
+            // @todo la recréer ? envoyer un message
+            throw new InsuranceNotFoundException();
+        }
+
+        return $attributeGroupId;
+    }
+
+    /**
+     * @param string $insuranceName
+     * @param int $attributeGroupId
+     * @param int $languageId
+     * @return int
+     */
+    public function createOrGetInsuranceAttributeId($insuranceName, $attributeGroupId, $languageId)
+    {
+        $insuranceAttributeId = $this->attributeRepository->getAttributeIdByNameAndGroup(
+            $insuranceName,
+            $attributeGroupId,
+            $languageId
+        );
+
+        if(!$insuranceAttributeId) {
+            /**
+             * @var \AttributeCore $testNewAttribute
+             */
+            $insuranceAttribute = new \AttributeCore();
+
+            // @todo fix error statuc
+            $insuranceAttribute->name = \PrestaShop\PrestaShop\Adapter\Import\ImportDataFormatter::createMultiLangField($insuranceName);
+            $insuranceAttribute->id_attribute_group = $attributeGroupId;
+            $insuranceAttribute->add();
+
+            $insuranceAttributeId = $insuranceAttribute->id;
+        }
+
+        return $insuranceAttributeId;
+    }
+
+    /**
+     * @param \ProductCore $defaultInsuranceProduct
+     * @param string $insuranceName
+     * @param float $insurancePrice
+     * @param int $insuranceAttributeId
+     * @return \CombinationCore
+     */
+    public function createOrGetInsuranceCombination($defaultInsuranceProduct, $insuranceName, $insurancePrice, $insuranceAttributeId)
+    {
+        // Check if the combination already exists
+        /**
+         * @var \CombinationCore $combination
+         */
+        $idProductAttribute =   \CombinationCore::getIdByReference($defaultInsuranceProduct->id, $insuranceName);
+
+        if(! $idProductAttribute) {
+            $idProductAttribute = $defaultInsuranceProduct->addCombinationEntity(
+                $insurancePrice,
+                $insurancePrice,
+                0,
+                1,
+                0,
+                1,
+                0, // @todo fix
+                $insuranceName,
+                0,
+                '',
+                0
+            );
+        }
+
+        $combination = new \CombinationCore((int)$idProductAttribute);
+        $combination->setAttributes(array($insuranceAttributeId));
+
+        return $combination;
     }
 }
