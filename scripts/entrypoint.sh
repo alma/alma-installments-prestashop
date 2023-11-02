e74583dcfdabae8d935830d84e79660d22b0c04b
Rémi Zuffinetti <remi.zuffinetti@getalma.eu>
2023-11-02 11:36:37 +0100

    Bash flags

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+########################################
+# Exit as soon as any line in the bash script fails.
+set -o errexit
+
+# pipefail: the return value of a pipeline is the status of the last command to exit with a non-zero status, or zero if no command exited with a non-zero status
+set -o pipefail
+
 bash /tmp/docker_install.sh
 
 exec "$@"
