2c8428c8721384ec50f72875a9363abad13cf0d9
Benjamin Freoua <benjamin.freoua@getalma.eu>
2024-01-02 11:31:18 +0100

    Remove change number of input when you wheel (scroll) inside the input

--- alma/views/js/alma-cart-insurance.js
+++ alma/views/js/alma-cart-insurance.js
@@ -120,6 +120,10 @@ function onloadInsuranceItemCartAlma() {
             let formQty = item.querySelector('.qty');
 
             formQty.querySelector('input').readOnly = true;
+            // Remove change number of input when you wheel (scroll) inside the input
+            formQty.querySelector('input').addEventListener('wheel', function (e) {
+                document.activeElement.blur();
+            });
             formQty.querySelector('.input-group-btn-vertical').remove();
         }
         if (isAlmaInsuranceProduct) {
