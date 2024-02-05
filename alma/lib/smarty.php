b1903d351188ebf6d0d72e2d82a3d06dc4371e7e
Benjamin Freoua <benjamin.freoua@getalma.eu>
2024-02-05 10:40:01 +0100

    sonar: Use the PHP_SAPI constant instead.

--- alma/lib/smarty.php
+++ alma/lib/smarty.php
@@ -25,7 +25,7 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-if (class_exists('\Context') && php_sapi_name() != 'cli') {
+if (class_exists('\Context') && PHP_SAPI != 'cli') {
     $smarty = \Context::getContext()->smarty;
 
     /**
