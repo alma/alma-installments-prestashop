4892a9fcb343ddf7e9aca731d4fbdf5f725a8308
Benjamin Freoua <benjamin.freoua@getalma.eu>
2024-05-03 18:31:25 +0200

    review aqua

--- alma/views/js/alma-insurance.js
+++ alma/views/js/alma-insurance.js
@@ -14,7 +14,7 @@
                 }
             })
             // TODO : Need to edit the url for remove an insurance product
-            //<a data-alma-association-id="14" data-action="remove-insurance-product" data-token="48fa711ffbafbe349ad4751b4015ad16" href="#" class="alma-remove-insurance-product" data-link="https://sync-ipln.serverapps.io/module/alma/insurance?action=removeInsuranceProduct">
+            //<a data-alma-association-id="14" data-action="remove-insurance-product" data-token="" href="#" class="alma-remove-insurance-product" data-link="https://sync-ipln.serverapps.io/module/alma/insurance?action=removeInsuranceProduct">
             //                                     Retirer l'assurance
             // 								</a>
         }
