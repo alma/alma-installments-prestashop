c58837f9d6b5da610a3cdb3bee182ca3d452457b
Benjamin Freoua <benjamin.freoua@getalma.eu>
2024-05-07 14:53:18 +0200

    WIP: Fix of minicart for insurance after updateCart

--- alma/views/js/alma-insurance.js
+++ alma/views/js/alma-insurance.js
@@ -1,20 +1,24 @@
 (function ($) {
     $(function () {
         if ($('.cartdrop-overview').length) {
-            const insuranceId = $('#alma-insurance-global').data('insurance-id')
-            $('.cartdrop-overview .productcard').each(function (i, e) {
-                if ($(e).hasClass(`cart-item-${insuranceId}`)) {
-                    $(e).find('input').prop('disabled', true)
-                    // Remove change number of input when you wheel (scroll) inside the input
-                    e.querySelector('input').addEventListener('wheel', function (e) {
-                        document.activeElement.blur();
-                    });
-                    $(e).find('.pscartdropdown-product-line-grid-right .input-group-btn-vertical').remove()
-                    $(e).find('a.label').removeAttr('href')
-                    // TODO : Need to edit the url of delete product for remove an insurance product
-                    $(e).find('.remove-from-cart').remove()
-                }
-            })
+            prestashop.on(
+                'updateCart',
+                function(event) {
+                    const insuranceId = $('#alma-insurance-global').data('insurance-id')
+                    $('.cartdrop-overview .productcard').each(function (i, e) {
+                        if ($(e).hasClass(`cart-item-${insuranceId}`)) {
+                            $(e).find('input').prop('disabled', true)
+                            // Remove change number of input when you wheel (scroll) inside the input
+                            e.querySelector('input').addEventListener('wheel', function (e) {
+                                document.activeElement.blur();
+                            });
+                            $(e).find('.pscartdropdown-product-line-grid-right .input-group-btn-vertical').remove()
+                            $(e).find('a.label').removeAttr('href')
+                            // TODO : Need to edit the url of delete product for remove an insurance product
+                            $(e).find('.remove-from-cart').remove()
+                        }
+                    })
+                });
         }
     });
 })(jQuery)
