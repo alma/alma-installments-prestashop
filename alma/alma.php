f98cde5223824a31728bac5ec1c5a0c8bce5c5cc
Claire <claire.bonachera@getalma.eu>
2024-05-22 10:40:34 +0200

    deactivate ps account

--- alma/alma.php
+++ alma/alma.php
@@ -148,6 +148,9 @@ class Alma extends PaymentModule
      */
     public function checkCompatibilityPSModule()
     {
+        // todo remove deactive ps account module
+        return false;
+
         if (
             $this->toolsHelper->psVersionCompare('1.6', '<')
             || !class_exists(\PrestaShop\ModuleLibServiceContainer\DependencyInjection\ServiceContainer::class)
