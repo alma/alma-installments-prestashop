b06dd3439b3e8a41e6c38bfacb613ea434d03f2d
Claire <claire.bonachera@getalma.eu>
2024-06-03 12:03:25 +0200

    fix rendering error

--- alma/controllers/hook/GetContentHookController.php
+++ alma/controllers/hook/GetContentHookController.php
@@ -860,7 +860,7 @@ final class GetContentHookController extends AdminHookController
             $this->context->smarty->assign([
                 'updated' => false,
             ]);
-            $messages = $this->module->display($this->module->file, 'getContent.tpl');
+            $messages = '';
         }
 
         $htmlForm = $this->renderForm();
