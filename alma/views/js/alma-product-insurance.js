48dda19555f08c47aecac8c4348d03278658db48
Benjamin Freoua <benjamin.freoua@getalma.eu>
2024-09-17 17:48:04 +0200

    fix: add verification in insurance js if add to cart button doesn't exist

--- alma/views/js/alma-product-insurance.js
+++ alma/views/js/alma-product-insurance.js
@@ -249,9 +249,11 @@ function addModalListenerToAddToCart() {
         if (!addToCart) {
             addToCart = document.querySelector('.add-to-cart a, .add-to-cart button');
         }
-        // If we change the quantity the DOM is reloaded then we need to remove and add the listener again
-        addToCart.removeEventListener("click", insuranceListener);
-        addToCart.addEventListener("click", insuranceListener);
+        if (addToCart) {
+            // If we change the quantity the DOM is reloaded then we need to remove and add the listener again
+            addToCart.removeEventListener("click", insuranceListener);
+            addToCart.addEventListener("click", insuranceListener);
+        }
     }
 }
 function insuranceListener(event) {
