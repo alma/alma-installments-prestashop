ed797d825c82f678753776bfb89458bd9f2a55e4
Francois-Gomis <francois.gomis@getalma.eu>
2024-10-21 15:34:49 +0200

    feat: add data in controller response

--- alma/tests/Unit/Controllers/Front/configurationExportTest.php
+++ alma/tests/Unit/Controllers/Front/configurationExportTest.php
@@ -2,9 +2,38 @@
 
 namespace Alma\PrestaShop\Tests\Unit\Controllers\Front;
 
+use Alma\PrestaShop\Helpers\ConfigurationHelper;
 use PHPUnit\Framework\TestCase;
 
+include __DIR__ . '/../../../../controllers/front/configexport.php';
 class AlmaConfigurationExportTestModuleFrontController extends TestCase
 {
+    protected $configHelper;
+    protected $moduleFrontController;
+    protected function setUp()
+    {
+        $this->configHelper = $this->createMock(ConfigurationHelper::class);
+        $this->moduleFrontController =  \Mockery::mock(\AlmaConfigExportModuleFrontController::class)->makePartial();
+        $this->moduleFrontController->setConfigHelper($this->configHelper);
 
+    }
+    protected function tearDown()
+    {
+        \Mockery::close();
+    }
+
+    public function testModule()
+    {
+        $this->assertInstanceOf(\AlmaConfigExportModuleFrontController::class, $this->moduleFrontController);
+    }
+
+    public function testConfigHelperSet()
+    {
+        $this->configHelper->expects($this->once())
+            ->method('get')
+            ->with('TEST_DATA')
+            ->willReturn('test');
+
+        $this->assertEquals('test', $this->moduleFrontController->getTestData());
+    }
 }
