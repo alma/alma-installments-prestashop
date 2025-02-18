d2e2daa02c9c42368a780d3aeae5ee479c25207f
Martin Supiot <martin.supiot@getalma.eu>
2025-02-18 10:02:38 +0100

    fix: maj pre-commit + add php compatibility

--- scripts/php-compatibility.sh
+++ scripts/php-compatibility.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-PROJECT_ROOT=$(git rev-parse --show-toplevel)
-docker run --rm -v "$PROJECT_ROOT/alma:/app/alma" phpcs:ps --standard=PHPCompatibility -s --runtime-set testVersion 5.6-8.1 ./alma
+docker run --rm -v "$(pwd)/alma:/app/alma" -v "$(pwd)/.php_cs.cache:/app/.php_cs.cache" --entrypoint /composer/vendor/bin/phpcs \
+  lint:ps -p alma --standard=PHPCompatibility -s --runtime-set testVersion 5.6-8.1 --ignore=\*/vendor/\*,\*/.coverage/\*
 EXIT_CODE=$?
+
 if [[ $EXIT_CODE -ne 0 ]]; then
     echo "Check PHP code compatibility before commit!"
     exit 1
