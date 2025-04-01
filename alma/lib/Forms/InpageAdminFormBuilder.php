1b4a3200212207e7277c08866444e2596e168705
Martin Supiot <martin.supiot@getalma.eu>
2025-04-01 16:06:23 +0200

    feat: set inpage by default on activation

--- alma/lib/Forms/InpageAdminFormBuilder.php
+++ alma/lib/Forms/InpageAdminFormBuilder.php
@@ -33,6 +33,7 @@ if (!defined('_PS_VERSION_')) {
  */
 class InpageAdminFormBuilder extends AbstractAlmaAdminFormBuilder
 {
+    const ALMA_INPAGE_DEFAULT_VALUE = true;
     const ALMA_ACTIVATE_INPAGE = 'ALMA_ACTIVATE_INPAGE';
     const ALMA_INPAGE_PAYMENT_BUTTON_SELECTOR = 'ALMA_INPAGE_PAYMENT_BUTTON_SELECTOR';
     const ALMA_INPAGE_DEFAULT_VALUE_PAYMENT_BUTTON_SELECTOR = '[data-module-name=alma]';
--- alma/lib/Helpers/SettingsHelper.php
+++ alma/lib/Helpers/SettingsHelper.php
@@ -539,7 +539,7 @@ class SettingsHelper
      */
     public function isInPageEnabled()
     {
-        return (bool) (int) $this->getKey(InpageAdminFormBuilder::ALMA_ACTIVATE_INPAGE, false);
+        return (bool) (int) $this->getKey(InpageAdminFormBuilder::ALMA_ACTIVATE_INPAGE, InpageAdminFormBuilder::ALMA_INPAGE_DEFAULT_VALUE);
     }
 
     /**
