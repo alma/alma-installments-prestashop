7cf4674caf56ed5f498a88dc8c8fbfd669acac34
Benjamin Freoua <benjamin.freoua@getalma.eu>
2026-02-17 10:07:25 +0100

    feat: improve encryptionHelper

--- alma/tests/Unit/Application/Helper/EncryptionHelperTest.php
+++ alma/tests/Unit/Application/Helper/EncryptionHelperTest.php
@@ -7,6 +7,19 @@ use PrestaShop\Module\Alma\Application\Helper\EncryptionHelper;
 
 class EncryptionHelperTest extends TestCase
 {
+    /**
+     * @var EncryptionHelper
+     */
+    private EncryptionHelper $encryptionHelper;
+
+    public function setUp(): void
+    {
+        $this->phpEncryption = $this->createMock(\PhpEncryption::class);
+        $this->encryptionHelper = new EncryptionHelper(
+            $this->phpEncryption
+        );
+    }
+
     public function testCheckIfIsEncryptionValueWithValueReturnTrue(): void
     {
         $this->assertTrue(EncryptionHelper::isEncryptionValue(true, 'value'));
@@ -25,5 +38,18 @@ class EncryptionHelperTest extends TestCase
     public function testCheckIfIsNotEncryptionValueWithoutValueReturnFalse(): void
     {
         $this->assertFalse(EncryptionHelper::isEncryptionValue(false, ''));
+    }
+
+    public function testEncryptionValue(): void
+    {
+        $value = 'test';
+        $encryptedValue = 'encrypted_test';
+
+        $this->phpEncryption->expects($this->once())
+            ->method('encrypt')
+            ->with($value)
+            ->willReturn($encryptedValue);
+
+        $this->assertEquals($encryptedValue, $this->encryptionHelper->encrypt($value));
     }
 }
