19ca6258403cc8998d3c0e0b039e7e5c6c4a400c
Benjamin Freoua <benjamin.freoua@getalma.eu>
2026-03-13 20:19:03 +0100

    feat: Get the ALMA_FEE_PLAN_LIST for form values

--- alma/src/Application/Assembler/FeePlanListAssembler.php
+++ alma/src/Application/Assembler/FeePlanListAssembler.php
@@ -4,7 +4,6 @@ namespace PrestaShop\Module\Alma\Application\Assembler;
 
 use Alma\Client\Domain\Entity\FeePlan;
 use PrestaShop\Module\Alma\Application\Provider\FeePlansProvider;
-use PrestaShop\Module\Alma\Infrastructure\Form\FeePlansAdminForm;
 use PrestaShop\Module\Alma\Infrastructure\Repository\ConfigurationRepository;
 
 class FeePlanListAssembler
@@ -42,11 +41,12 @@ class FeePlanListAssembler
     public function assemble(): array
     {
         $feePlanList = $this->feePlansProvider->getFeePlanList();
+        $feePlanConfiguration = $this->feePlansProvider->getFeePlanFromConfiguration();
         $feePlanListAssembled = [];
         /** @var FeePlan $feePlan */
         // TODO : Can we add optional fields (enabled and sortOrder) in the FeePlan entity to keep object FeePlan
         foreach ($feePlanList as $feePlan) {
-            $planKey = mb_strtoupper($feePlan->getPlanKey());
+            $planKey = $feePlan->getPlanKey();
             $feePlanListAssembled[] = [
                 'allowed' => $feePlan->isAllowed(),
                 'available_online' => $feePlan->isAvailableOnline(),
@@ -59,8 +59,8 @@ class FeePlanListAssembler
                 'merchant_fee_variable' => $feePlan->getMerchantFeeVariable(),
                 'merchant_fee_fixed' => $feePlan->getMerchantFeeFixed(),
                 'min_purchase_amount' => $feePlan->getMinPurchaseAmount(),
-                'enabled' => (bool) $this->configurationRepository->get(sprintf(FeePlansAdminForm::KEY_FIELD_FEE_PLAN_STATE, $planKey)),
-                'sort_order' => (int) $this->configurationRepository->get(sprintf(FeePlansAdminForm::KEY_FIELD_FEE_PLAN_SORT_ORDER, $planKey)),
+                'enabled' => isset($feePlanConfiguration[$planKey]) && (bool) $feePlanConfiguration[$planKey]['state'],
+                'sort_order' => isset($feePlanConfiguration[$planKey]) ? (int) $feePlanConfiguration[$planKey]['sort_order'] : 0,
             ];
         }
         return $feePlanListAssembled;
