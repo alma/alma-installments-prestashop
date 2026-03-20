52ae4e430d67f65a1e4e198e8b6a4af0c1099100
Benjamin Freoua <benjamin.freoua@getalma.eu>
2026-03-20 19:12:20 +0100

    feat: improve service categorie exclude + fix bulk action

--- alma/src/Infrastructure/Controller/ExcludedCategoriesController.php
+++ alma/src/Infrastructure/Controller/ExcludedCategoriesController.php
@@ -27,4 +27,9 @@ class ExcludedCategoriesController extends FrameworkBundleAdminController
     {
         return $this->redirectToRoute('alma_excluded_categories');
     }
+
+    public function bulkExcludeAction(Request $request): RedirectResponse
+    {
+        return $this->redirectToRoute('alma_excluded_categories');
+    }
 }
