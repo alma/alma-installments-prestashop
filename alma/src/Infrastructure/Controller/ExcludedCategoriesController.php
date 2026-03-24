01ec0ac76186ffbd3159a3be3b5213aa753186bd
Benjamin Freoua <benjamin.freoua@getalma.eu>
2026-03-24 14:48:58 +0100

    feat: remove searchAction route + controller

--- alma/src/Infrastructure/Controller/ExcludedCategoriesController.php
+++ alma/src/Infrastructure/Controller/ExcludedCategoriesController.php
@@ -4,7 +4,6 @@ namespace PrestaShop\Module\Alma\Infrastructure\Controller;
 
 use PrestaShop\Module\Alma\Infrastructure\Grid\Filter\CategoryFilters;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
-use PrestaShopBundle\Service\Grid\ResponseBuilder;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,19 +20,6 @@ class ExcludedCategoriesController extends FrameworkBundleAdminController
         return $this->render(
             '@Modules/alma/views/templates/admin/excluded_categories.html.twig',
             ['categoryGrid' => $this->presentGrid($categoryGrid)]
-        );
-    }
-
-    public function searchAction(Request $request): RedirectResponse
-    {
-        /** @var ResponseBuilder $responseBuilder */
-        $responseBuilder = $this->get('prestashop.bundle.grid.response_builder');
-
-        return $responseBuilder->buildSearchResponse(
-            $this->get('alma.category_grid_definition_factory'),
-            $request,
-            'categories',
-            'alma_excluded_categories'
         );
     }
 
