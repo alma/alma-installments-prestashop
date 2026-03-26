d3c6c8c5f741cc9109e92728f5a6ec70e401ddf5
Benjamin Freoua <benjamin.freoua@getalma.eu>
2026-03-26 19:06:03 +0100

    feat: Add Jest Test for unit test dom js code

--- alma/views/js/alma-widget.js
+++ alma/views/js/alma-widget.js
@@ -1,25 +1,41 @@
-(function ($) {
-    $(function () {
-        if (!$("#alma-widget-ShoppingCartFooter").length && !$("#alma-widget-cart").length) {
-            return;
-        }
-        let widgetConfig = $("#alma-widget-ShoppingCartFooter").data('widget-config');
-        if ($("#alma-widget-cart").length) {
-            widgetConfig = $("#alma-widget-cart").data('widget-config');
-        }
+function initAlmaWidget($, Alma) {
+    if (!$("#alma-widget-ShoppingCartFooter").length && !$("#alma-widget-cart").length) {
+        return null;
+    }
+    let widgetConfig = $("#alma-widget-ShoppingCartFooter").data('widget-config');
+    if ($("#alma-widget-cart").length) {
+        widgetConfig = $("#alma-widget-cart").data('widget-config');
+    }
 
-        const mode = (widgetConfig.mode === 'test') ? Alma.ApiMode.TEST : Alma.ApiMode.LIVE
-        const widgets = Alma.Widgets.initialize(
-            widgetConfig.merchantId,
-            mode,
-        )
+    const mode = (widgetConfig.mode === 'test') ? Alma.ApiMode.TEST : Alma.ApiMode.LIVE
+    const widgets = Alma.Widgets.initialize(
+        widgetConfig.merchantId,
+        mode,
+    )
 
-        widgets.add(Alma.Widgets.PaymentPlans, {
-            container: widgetConfig.containerId,
-            purchaseAmount: widgetConfig.purchaseAmount,
-            locale: widgetConfig.locale,
-            hideIfNotEligible: widgetConfig.hideIfNotEligible,
-            plans: widgetConfig.plans,
-        })
-    });
-})(jQuery);
+    if (typeof widgetConfig.plans === 'string') {
+        widgetConfig.plans = JSON.parse(widgetConfig.plans);
+    }
+
+    widgets.add(Alma.Widgets.PaymentPlans, {
+        container: widgetConfig.containerId,
+        purchaseAmount: widgetConfig.purchaseAmount,
+        locale: widgetConfig.locale,
+        hideIfNotEligible: widgetConfig.hideIfNotEligible,
+        plans: widgetConfig.plans,
+    })
+
+    return widgets;
+}
+
+if (typeof module !== 'undefined') {
+    console.log('Exporting initAlmaWidget for testing');
+    module.exports = { initAlmaWidget };
+} else {
+    console.log('Initializing Alma Widget on page load');
+    (function ($) {
+        $(function () {
+            initAlmaWidget($, Alma);
+        });
+    })(jQuery);
+}
