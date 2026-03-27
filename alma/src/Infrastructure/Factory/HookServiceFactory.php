a9aa59e62067b8491c6d37429fdc8d35f562acf8
Benjamin Freoua <benjamin.freoua@getalma.eu>
2026-03-27 17:35:37 +0100

    feat: feature/ecom-3961-handle-the-exclude-category-displayed

--- alma/src/Infrastructure/Factory/HookServiceFactory.php
+++ alma/src/Infrastructure/Factory/HookServiceFactory.php
@@ -2,16 +2,32 @@
 
 namespace PrestaShop\Module\Alma\Infrastructure\Factory;
 
+use PrestaShop\Module\Alma\Application\Service\ExcludedCategoriesService;
 use PrestaShop\Module\Alma\Application\Service\WidgetFrontendService;
+use PrestaShop\Module\Alma\Infrastructure\Proxy\ProductProxy;
 use PrestaShop\Module\Alma\Infrastructure\Repository\ConfigurationRepository;
+use PrestaShop\Module\Alma\Infrastructure\Repository\ExcludedCategoriesRepository;
+use PrestaShop\Module\Alma\Infrastructure\Repository\LanguageRepository;
 
 class HookServiceFactory
 {
     public static function createWidgetService(\Context $context): WidgetFrontendService
     {
+        $configurationRepository = new ConfigurationRepository();
+        $excludedCategoriesRepository = new ExcludedCategoriesRepository($configurationRepository);
+        $excludedCategoriesService = new ExcludedCategoriesService(
+            $context,
+            $excludedCategoriesRepository,
+            $configurationRepository,
+            new LanguageRepository(),
+            $context->getTranslator(),
+            new ProductProxy()
+        );
+
         return new WidgetFrontendService(
             $context,
-            new ConfigurationRepository()
+            $configurationRepository,
+            $excludedCategoriesService
         );
     }
 }
--- alma/views/templates/widget/cart.tpl
+++ alma/views/templates/widget/cart.tpl
@@ -1,1 +1,8 @@
-<div id="{$container}" data-widget-config="{$widgetConfig}"></div>
+{if !$isExcluded}
+    <div id="{$container}" data-widget-config="{$widgetConfig}"></div>
+{elseif $showExcludedMessage}
+    <div class="alma-widget-excluded-message">
+        <img src="{$almaLogoUrl}" alt="Alma" />
+        <p>{$excludedMessage}</p>
+    </div>
+{/if}
